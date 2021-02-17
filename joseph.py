a6f0861eda375dc10555dbdad23e779eeaa4ab06
Thomas-ZYT <79186460+Thomas-ZYT@users.noreply.github.com>
2021-02-17 14:04:34 +0800

    Create joseph.py

---
+++ joseph.py
@@ -0,0 +1,51 @@
+# import pandas as pd
+# import numpy as np
+
+# new_joiner_source = "C:\\Users\\my_name\\Desktop\\new.csv"
+
+# df = pd.read_csv(new_joiner_source)
+# df.shape
+# df.head()
+# df.head(1)
+# df["column_name"].unique()
+# df["column_name"].value_counts()
+
+carrol.pd_tocsv(folder/desktop/new.csv)
+
+
+DataFrame.to_csv(file_path)
+
+for item in df["column_name"].head(10):
+    print(item.split(";"))
+
+# df["column_name"].str.contains("test_valu").value_counts()
+# filter = df["column_name"].str.contains("test_valu",na=False).value_counts()  
+# df[filter]
+# Boolean -> True / False
+
+# df["column_name"].unique().tolist()
+# df.isna().sum()
+# df.isna().mean()
+# df.isna().mean().sort_values()
+# df.isna().mean().sort_values()[-1]
+# df.isna().mean()*100
+# df.isna().mean().index
+# df.isna().mean().values
+# df.isna().mean().sort_values()[-10:]
+# df.isna().mean().sort_values(ascending=False)
+# df.isna().mean().sort_values(ascending=True)
+# round(df.insa().mean()*100,2)
+
+# [0:3] # from 0 to 3, excluding 3
+# [:] [:100] [10:]
+
+
+
+
+
+# 
+
+# print(df.head())
+
+
+print(1)
