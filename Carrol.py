39a8b94c8a3c33d5a3f0341d58387246b2dbd26b
Thomas-ZYT <79186460+Thomas-ZYT@users.noreply.github.com>
2021-02-17 14:01:50 +0800

    Create Carrol.py

---
+++ Carrol.py
@@ -0,0 +1,37 @@
+# def input variable
+
+carrol_1 = [1, 2, 3, 4, 5, ]
+carrol_2 = [1, 2, 3, 4, 5]
+
+
+carrol_3 = ['1', '2', '3', '4', '5', '6',
+            '7', '8', '9', '10', '11', '12', '13']
+
+
+Print(list(range(1, 31, 1)))
+
+# Data Type 数据类型
+# datetime
+
+"""
+
+numner: interger, float
+
+string (text):one, two ,three
+
+"""
+
+
+print(carrol_1 == carrol_2)
+
+print(carrol_1 is carrol_2)
+
+print(id(carrol_1))
+print(id(carrol_2))
+
+# function
+
+for num in carrol_1:
+    print(num)
+
+print(carrol_2[2])
